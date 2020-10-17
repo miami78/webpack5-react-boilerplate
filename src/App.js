30d4a06e7cfe4cc65ba07fd3f5f7fd4e05b4ba7c
miami78 <miamilarry78@gmail.com>
2020-10-17 08:44:55 +0300

    add entry point

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./app.styles.scss";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        Webpack 5 boilerplate for react using babel, sass, with a hot dev server
+        and an optimized production build.
+      </div>
+    );
+  }
+}
+export default App;
